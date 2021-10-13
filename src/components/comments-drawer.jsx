8864e9b900c0e3e04e400034831dee1f489dc98e
baucucu <alexandru.raduca@gmail.com>
2021-10-13 09:05:44 +0300

    comment testing

--- src/components/comments-drawer.jsx
+++ src/components/comments-drawer.jsx
@@ -1,25 +1,26 @@
 import React,{useEffect} from 'react'
+import qs from 'qs';
 import {Block} from 'framework7-react';
 const commentBox = window.commentBox;
 
 
 
 const CommentsDrawer = (props) => {
-  console.log("commentsbox id: ",props?.commentBoxId);
+  
   useEffect(() => {    
+    console.log("commentsbox id: ",props?.commentBoxId);
     commentBox('5707536883777536-proj', {
       createBoxUrl(boxId, pageLocation) {
-
         const relevantParams = {
-            'commentBoxId': props.commentBoxId
+            'commentboxId': props.commentBoxId
         };
-        pageLocation.search = encodeURIComponent(relevantParams); // we will now include "?page_id=5" in the box URL
+        pageLocation.search = qs.stringify(relevantParams); // we will now include "?page_id=5" in the box URL
         pageLocation.hash = boxId; // creates link to this specific Comment Box on your page
         return pageLocation.href; // return url string
       }
-    }
-    );
+    })
   },[])
+  
   return (
     <div className="commentbox"/>
 );
