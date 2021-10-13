405b2eb089e52d0d1fe5428a5c47d578279887eb
baucucu <alexandru.raduca@gmail.com>
2021-10-13 09:42:54 +0300

    update

--- src/components/comments-drawer.jsx
+++ src/components/comments-drawer.jsx
@@ -12,18 +12,7 @@ const CommentsDrawer = (props) => {
   },[props])
 
   useEffect(() => {    
-   const removeCommentBox = commentBox('5707536883777536-proj', {
-      createBoxUrl(boxId, pageLocation) {
-        console.log("commentBox running: ", boxId, pageLocation)
-        const relevantParams = {
-            'commentboxId': props.commentBoxId
-        };
-        pageLocation.search = qs.stringify(relevantParams); // we will now include "?page_id=5" in the box URL
-        pageLocation.hash = boxId; // creates link to this specific Comment Box on your page
-        return pageLocation.href; // return url string
-      }
-    })
-
+   const removeCommentBox = commentBox('5707536883777536-proj')
     return removeCommentBox()
   },[])
 
