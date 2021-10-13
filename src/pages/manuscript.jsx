47faf51df8ba59fe80fd386e0a94c2c1570f972c
baucucu <alexandru.raduca@gmail.com>
2021-10-13 11:05:36 +0300

    ui update

--- src/pages/manuscript.jsx
+++ src/pages/manuscript.jsx
@@ -35,47 +35,41 @@ const ManuscriptPage = () => {
     <Stack direction="row">
       <Block inset strong style={{flexGrow:1, marginTop:0}}>
         <Stack direction="row" spacing={3}>
-          <Stack mr={3} sx={{flexDirection:"row", justifyContent: "space-between", alignItems: "center"}}>
+          <Stack direction="row">
             <Typography mr={1} variant="subtitle1" color="text.secondary" component="div">Word count</Typography>
             <Chip color="secondary" variant="outlined" label={"385"} ></Chip>
           </Stack>
-          <Stack mr={3} sx={{flexDirection:"row", justifyContent: "space-between", alignItems: "center"}}>
-            <Typography mr={1} variant="subtitle1" color="text.secondary" component="div">Target length</Typography>
+          <Stack direction="row" spacing={1}>
+            <Typography variant="subtitle1" color="text.secondary" component="div">Target length</Typography>
             <Chip color="secondary" variant="outlined" sx={{marginRight:1}} label={"60s"} ></Chip>
             <Chip color="secondary" variant="outlined" label={"not so strict"} ></Chip>
           </Stack>
-          <Stack mr={3} sx={{flexDirection:"row", justifyContent: "space-between", alignItems: "center"}}>
-            <Typography mr={1} variant="subtitle1" color="text.secondary" component="div">Scenes</Typography>
+          <Stack direction="row" spacing={1}>
+            <Typography  variant="subtitle1" color="text.secondary" component="div">Scenes</Typography>
             <Chip color="secondary" variant="outlined" label={"8"} ></Chip>
           </Stack>
         </Stack>
         <Stack direction="row" mt={2}>
-            <Chip clickable color="secondary" variant="filled " style={{marginLeft:8}} icon={<VolumeUpIcon/>} label="Listen to AI Voiceover" />
-            <Chip clickable color="secondary" variant="filled " style={{marginLeft:8}} icon={<PictureAsPdfIcon/>} label="Download PDF" />
-            {/* <CardButton color="secondary" size="small" onClick={()=>f7.emit('listenVoiceover')}>Listen to AI Voiceover</CardButton>
-            <CardButton color="secondary" size="small" onClick={()=>f7.emit('downloadPDF')}>Download PDF</CardButton> */}
+            <Chip clickable color="info" variant="filled " style={{marginLeft:8}} icon={<VolumeUpIcon/>} label="Listen to AI Voiceover" />
+            <Chip clickable color="info" variant="filled " style={{marginLeft:8}} icon={<PictureAsPdfIcon/>} label="Download PDF" />
         </Stack>
       </Block>
       <Block inset strong style={{flexGrow:1, marginTop:0}}>
         <Stack direction="row" spacing={2}>
-          <Stack sx={{flexDirection:"row", justifyContent: "space-between", alignItems: "center"}}>
-              <Typography mr={1} variant="subtitle1" color="text.secondary" component="div">Revisions</Typography>
+          <Stack direction="row" spacing={1}>
+              <Typography variant="subtitle1" color="text.secondary" component="div">Revisions</Typography>
               <Chip color="secondary" variant="outlined" label={"2/5"} ></Chip>
           </Stack>
-          <Stack sx={{flexDirection:"row", justifyContent: "space-between", alignItems: "center"}}>
-              <Typography mr={1} variant="subtitle1" color="text.secondary" component="div">Time to respond</Typography>
+          <Stack direction="row" spacing={1}>
+              <Typography variant="subtitle1" color="text.secondary" component="div">Time to respond</Typography>
               <Chip color="secondary" variant="outlined" label={"18:00:00"} ></Chip>
           </Stack>
         </Stack>
         <Stack direction="row" spacing={2} mt={2}>
-          <Chip onClick={()=>f7.dialog.confirm('Are you sure you want to send to client?')} clickable color="primary" variant="filled " style={{marginLeft:8}} icon={<SendIcon pl={0} />} label="Send to client" />
+          <Chip onClick={()=>f7.dialog.confirm('Are you sure you want to send to client?')} clickable color="success" variant="filled " style={{marginLeft:8}} icon={<SendIcon pl={0} />} label="Send to client" />
           <Chip onClick={()=>f7.dialog.confirm('Are you sure you want to approve the manuscript?')} clickable color="success" variant="filled " style={{marginLeft:8}} icon={<CheckIcon/>} label="Approve manuscript" />
-          <Chip onClick={()=>f7.dialog.confirm('Are you sure you want to ask for revision?')} clickable color="warning" variant="filled " style={{marginLeft:8}} icon={<RateReviewIcon/>} label="Ask for revision" />
+          <Chip onClick={()=>f7.dialog.confirm('Are you sure you want to ask for revision?')} clickable color="info" variant="filled " style={{marginLeft:8}} icon={<RateReviewIcon/>} label="Ask for revision" />
           <Chip onClick={()=>f7.dialog.confirm('Are you sure you want to extend time?')} clickable color="warning" variant="filled " style={{marginLeft:8}} icon={<MoreTimeIcon/>} label="Extend time" />
-          {/* <CardButton color="secondary" size="small" onClick={()=>f7.dialog.confirm('Are you sure you want to send to client?')}>Send to client</CardButton>
-          <CardButton color="secondary" size="small" onClick={()=>f7.dialog.confirm('Are you sure you want to approve the manuscript?')}>Approve manuscript</CardButton>
-          <CardButton color="secondary" size="small" onClick={()=>f7.dialog.confirm('Are you sure you want to ask for revision?')}>Ask for revision</CardButton>
-          <CardButton color="secondary" size="small" onClick={()=>f7.dialog.confirm('Are you sure you want to extend time?')}>Extend time</CardButton> */}
         </Stack>
       </Block>
     </Stack>
