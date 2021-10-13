9b6ac7073003d050025b5d718cb735d0d16439a8
baucucu <alexandru.raduca@gmail.com>
2021-10-13 08:41:07 +0300

    update

--- src/components/comments-drawer.jsx
+++ src/components/comments-drawer.jsx
@@ -2,9 +2,10 @@ import React,{useEffect} from 'react'
 import {Block} from 'framework7-react';
 const commentBox = window.commentBox;
 
-console.log("commentsbox id: ",props?.commentBoxId);
+
 
 const CommentsDrawer = (props) => {
+  console.log("commentsbox id: ",props?.commentBoxId);
   useEffect(() => {    
     commentBox('5707536883777536-proj');
   },[])
