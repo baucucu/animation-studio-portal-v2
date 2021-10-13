a94e45124ecbee1d53920a13d4b64102a15338cb
baucucu <alexandru.raduca@gmail.com>
2021-10-13 08:12:59 +0300

    comments testing

--- src/components/comments-drawer.jsx
+++ src/components/comments-drawer.jsx
@@ -11,7 +11,7 @@ const CommentsDrawer = (props) => {
   },[])
   return (
     // <div className="commentbox" />
-    <iframe src={`https://app.commentbox.io/5707536883777536-proj?id=commentbox&amp;url=https%3A%2F%2Fwww.studioproject.io%2F%23${props.id}&amp;tlc_param=tlc&amp;background_color=&amp;text_color=&amp;subtext_color=&amp;sort_order=best`} frameborder="0" scrolling="no" style="width: 100%" data-comments-loaded="true" height="1378px"></iframe>
+    <iframe src={`https://app.commentbox.io/5707536883777536-proj?id=commentbox&amp;url=https%3A%2F%2Fwww.studioproject.io%2F%23${props.id}&amp;tlc_param=tlc&amp;background_color=&amp;text_color=&amp;subtext_color=&amp;sort_order=best`} frameborder="0" scrolling="no" style={{width: "100%"}} height="1378px"></iframe>
 );
 }
 
