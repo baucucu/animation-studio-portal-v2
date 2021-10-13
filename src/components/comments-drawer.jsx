970d91f82066e9cf20a7e65de91349f7b7daaaf4
baucucu <alexandru.raduca@gmail.com>
2021-10-13 09:27:32 +0300

    comments update

--- src/components/comments-drawer.jsx
+++ src/components/comments-drawer.jsx
@@ -1,28 +1,40 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 import qs from 'qs';
-import {Block} from 'framework7-react';
+import {f7} from 'framework7-react';
 const commentBox = window.commentBox;
 
 
 
 const CommentsDrawer = (props) => {
-  
+
+  const [commentBoxId, setCommentBoxId] = useState(props.commentBoxId)
+
+  useEffect(() => {
+    f7.on('showComments', (e) => {console.log("showComments event received:",e); setCommentBoxId(String(e.dealId)+"-"+String(e.sceneId))}) 
+  },[])
+
+  useEffect(() => {
+    console.log("commentBoxId changed: ", commentBoxId)
+  },[commentBoxId])
+
   useEffect(() => {    
-    console.log("commentsbox id: ",props?.commentBoxId);
-    commentBox('5707536883777536-proj', {
+   const removeCommentBox = commentBox('5707536883777536-proj', {
       createBoxUrl(boxId, pageLocation) {
+        console.log("commentBox running: ", boxId, pageLocation)
         const relevantParams = {
-            'commentboxId': props.commentBoxId
+            'commentboxId': commentBoxId
         };
         pageLocation.search = qs.stringify(relevantParams); // we will now include "?page_id=5" in the box URL
         pageLocation.hash = boxId; // creates link to this specific Comment Box on your page
         return pageLocation.href; // return url string
       }
     })
-  },[])
-  
+
+    return removeCommentBox()
+  },[commentBoxId])
+
   return (
-    <div className="commentbox"/>
+    <div className="commentbox"  id={commentBoxId} />
 );
 }
 
