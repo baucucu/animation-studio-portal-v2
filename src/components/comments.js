c85dfc5e6071055b2c7c034201ad6894329435d3
baucucu <alexandru.raduca@gmail.com>
2021-10-13 09:57:47 +0300

    update

---
+++ src/components/comments.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import commentBox from 'commentbox.io';
+
+class PageWithComments extends React.Component {
+
+    componentDidMount() {
+
+        this.removeCommentBox = commentBox('5707536883777536-proj');
+    }
+
+    componentWillUnmount() {
+
+        this.removeCommentBox();
+    }
+
+    render() {
+
+        return (
+            <div className="commentbox" id={this.props.commentBoxId}/>
+        );
+    }
+}
+
+export default PageWithComments
