3c290f436df03d459d7a3e7aaa269c9aab82af01
baucucu <alexandru.raduca@gmail.com>
2021-10-13 08:51:21 +0300

    testing comments

--- src/components/comments-drawer.jsx
+++ src/components/comments-drawer.jsx
@@ -7,10 +7,21 @@ const commentBox = window.commentBox;
 const CommentsDrawer = (props) => {
   console.log("commentsbox id: ",props?.commentBoxId);
   useEffect(() => {    
-    commentBox('5707536883777536-proj');
+    commentBox('5707536883777536-proj', {
+      createBoxUrl(boxId, pageLocation) {
+
+        const relevantParams = {
+            'commentBoxId': props.commentBoxId
+        };
+        pageLocation.search = qs.stringify(relevantParams); // we will now include "?page_id=5" in the box URL
+        pageLocation.hash = boxId; // creates link to this specific Comment Box on your page
+        return pageLocation.href; // return url string
+      }
+    }
+    );
   },[])
   return (
-    <div className="commentbox" id={props.commentBoxId}/>
+    <div className="commentbox"/>
 );
 }
 
