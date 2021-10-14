a8eb2c63cff9cd74623bfc9cf40a74fbd1fd4c1b
baucucu <alexandru.raduca@gmail.com>
2021-10-14 17:23:00 +0300

    ui updates. stable manuscript version

--- src/components/manuscript-scenes.jsx
+++ src/components/manuscript-scenes.jsx
@@ -9,12 +9,18 @@ import CardContent from '@mui/material/CardContent';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import Badge from '@mui/material/Badge';
-import MicIcon from '@mui/icons-material/Mic';
-import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
-import RateReviewIcon from '@mui/icons-material/RateReview';
 import IconButton from '@mui/material/IconButton';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
+
+import MicIcon from '@mui/icons-material/Mic';
+import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
+import RateReviewIcon from '@mui/icons-material/RateReview';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import AddIcon from '@mui/icons-material/Add';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+
 
 
 export default function ManuscriptScenes(props) {
@@ -27,7 +33,42 @@ export default function ManuscriptScenes(props) {
                 <Grid item key={id}>
                     <Card sx={{width: 450}}>
                         <CardContent >
-                        <Typography variant="h6" color="text.secondary" component="div">Scene {id+1}</Typography>
+                            <Stack direction="row" sx={{justifyContent:"space-between"}}>
+                                <Stack direction="row">
+                                    {id !==0 && <Box>
+                                        <IconButton>
+                                            <ArrowLeftIcon color="secondary"/>
+                                        </IconButton>
+                                    </Box>}
+                                    <Box>
+                                        <IconButton>
+                                            <AddIcon color="secondary"/>
+                                        </IconButton>
+                                    </Box>
+                                </Stack>
+                                <Stack direction="row" sx={{alignItems:"center",     justifyContent:"center"}}>
+                                    <Typography variant="h6" color="text.secondary" component="div">Scene {id+1}</Typography>
+                                    <Box>
+                                        <IconButton>
+                                            <HighlightOffIcon color="secondary"/>
+                                        </IconButton>
+                                    </Box>
+                                </Stack>
+                                
+                                <Stack direction="row">
+                                    <Box>
+                                        <IconButton>
+                                            <AddIcon color="secondary"/>
+                                        </IconButton>
+                                    </Box>
+                                    {id !== 4 && <Box>
+                                        <IconButton>
+                                            <ArrowRightIcon color="secondary"/>
+                                        </IconButton>
+                                    </Box>}
+                                </Stack>
+                            </Stack>
+                            
                             <Stack sx={{flexDirection:"column"}} spacing={2}>
                                 <Stack mt={1} sx={{flexDirection:"row"}}>
                                     <MicIcon />
--- src/pages/manuscript.jsx
+++ src/pages/manuscript.jsx
@@ -1,13 +1,15 @@
 import React, {useState} from 'react';
-import { Page, Icon, Block, BlockTitle, Segmented, Button,f7 } from 'framework7-react';
+import { Page, Icon, Block, Input, Segmented, Button,f7 } from 'framework7-react';
 
 import Chip from '@mui/material/Chip';
-import CardActions from '@mui/material/CardActions';
-import CardButton from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
 import MUIButton from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
 import VolumeUpIcon from '@mui/icons-material/VolumeUp';
@@ -18,19 +20,61 @@ import RateReviewIcon from '@mui/icons-material/RateReview';
 import MoreTimeIcon from '@mui/icons-material/MoreTime';
 import PreviewIcon from '@mui/icons-material/Preview';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+
 
 import ManuscriptScenes from '../components/manuscript-scenes'
 
 const ManuscriptPage = () => {
   
-  const [briefDrawerOpen, setBriefDrawerOpen] = useState(false)
-  const toggleBriefDrawer = () => {
-    setBriefDrawerOpen(!briefDrawerOpen)
-  }
+  const [version, setVersion] = useState(1)
+  const [language, setLanguage] = useState("English")
+  
+  const handleChangeVersion = (event) => {
+    setVersion(event.target.value);
+  };
 
   return (
   <Page className="viewPage">
-    <Stack direction="row">
+    <Stack direction="row" justifyContent="stretch">
+    <Block inset strong>
+        <Stack direction="row" spacing={2}>
+          <SplitButton />
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Version</InputLabel>
+              <Select
+                color="secondary"
+                size="small"
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={version}
+                label="Version"
+                onChange={handleChangeVersion}
+              >
+                <MenuItem value={1}>1 (current)</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Stack>
+        <Stack direction="row" mt={2} spacing={2}>
+          <Box>
+              <MUIButton size="small" variant="contained" color= "success" startIcon={<SendIcon />} onClick={()=>f7.dialog.confirm('Are you sure you want to send to client?')}>
+                Send to client
+              </MUIButton>
+            </Box>
+            <Box>
+              <MUIButton size="small" variant="contained" color= "warning" startIcon={<MoreTimeIcon />} onClick={()=>f7.dialog.confirm('Are you sure you want to extend time?')}>
+                Extend time
+              </MUIButton>
+            </Box>
+            <Box>
+            <Chip   variant="outlined" icon={<AccessTimeIcon/>} label="In review by client"/>
+            </Box>
+        </Stack>
+      </Block>
       <Block inset strong style={{flexGrow:1}}>
         <Stack direction="row" spacing={3}>
           <Stack direction="row">
@@ -47,7 +91,7 @@ const ManuscriptPage = () => {
             <Chip color="secondary" variant="outlined" label={"8"} ></Chip>
           </Stack>
         </Stack>
-        <Stack direction="row" mt={2} spacing={2}>
+        <Stack direction="row" mt={3} spacing={2}>
           <Box>
             <MUIButton size="small" variant="contained" color= "info" startIcon={<VolumeUpIcon />}>
               Listen to AI Voiceover
@@ -65,7 +109,7 @@ const ManuscriptPage = () => {
           </Box>
         </Stack>
       </Block>
-      <Block inset strong style={{flexGrow:1}}>
+      <Block inset strong style={{flexGrow:2}}>
         <Stack direction="row" spacing={2}>
           <Stack direction="row" spacing={1}>
               <Typography variant="subtitle1" color="text.secondary" component="div">Revisions</Typography>
@@ -76,35 +120,30 @@ const ManuscriptPage = () => {
               <Chip color="secondary" variant="outlined" label={"18:00:00"} ></Chip>
           </Stack>
         </Stack>
-        <Stack direction="row" spacing={2} mt={2}>
-          <Box>
-            <MUIButton size="small" variant="contained" color= "success" startIcon={<SendIcon />} onClick={()=>f7.dialog.confirm('Are you sure you want to send to client?')}>
-              Send to client
-            </MUIButton>
-          </Box>
+        <Stack direction="row" spacing={2} mt={3}>
+          
           <Box>
             <MUIButton size="small" variant="contained" color= "success" startIcon={<CheckIcon />} onClick={()=>f7.dialog.confirm('Are you sure you want to approve the manuscript?')}>
               Approve manuscript
             </MUIButton>
           </Box>
           <Box>
-            <MUIButton size="small" variant="contained" color= "info" startIcon={<RateReviewIcon />} onClick={()=>f7.dialog.confirm('Are you sure you want to ask for revision?')}>
+            <MUIButton size="small" variant="contained" color= "warning" startIcon={<RateReviewIcon />} onClick={()=>f7.dialog.confirm('Are you sure you want to ask for revision?')}>
               Ask for revision
             </MUIButton>
           </Box>
           <Box>
-            <MUIButton size="small" variant="contained" color= "warning" startIcon={<MoreTimeIcon />} onClick={()=>f7.dialog.confirm('Are you sure you want to extend time?')}>
-              Extend time
-            </MUIButton>
+            <Chip   variant="outlined" icon={<AccessTimeIcon/>} label="Open by manuscript writer"/>
           </Box>
+          
         </Stack>
       </Block>
     </Stack>
-    <Box sx={{display:"flex", flexDirection:"row"}}>
+    {/* <Box sx={{display:"flex", flexDirection:"row"}}>
       <Block inset style={{flexDirection:"row", marginBottom:16, marginTop:16, flexGrow:0, justifyContent:'flex-start'}}>
         <SplitButton />
       </Block>
-    </Box>
+    </Box> */}
     <Block inset >
       <ManuscriptScenes />
     </Block>
