5b16e62b86dd5b1715af8b435e3e03397e6b31c5
Alex <alexandru.raduca@gmail.com>
2021-10-27 23:25:44 +0300

    refactored manuscript meta component UI

--- src/pages/manuscript.jsx
+++ src/pages/manuscript.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Page, Chip, Block, BlockTitle, ListButton,Popover, List, Link, Segmented, Button,f7, useStore } from 'framework7-react';
+import { Page, Icon, Chip,ListItem, Block, BlockTitle, ListButton,Popover, List, Link, Segmented, Button,f7, useStore } from 'framework7-react';
 
 import MUIChip from '@mui/material/Chip';
 import Typography from '@mui/material/Typography';
@@ -12,6 +12,7 @@ import MenuList from '@mui/material/MenuList';
 import MenuItem from '@mui/material/MenuItem';
 
 import CheckIcon from '@mui/icons-material/Check';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import SendIcon from '@mui/icons-material/Send';
@@ -84,9 +85,9 @@ const ManuscriptPage = () => {
   else return (
   <Page className="viewPage">
     <Stack direction="row" justifyContent="stretch">
-      {user !== null && user?.customData?.role === "freelancer" && <FreelancerManuscriptControlPanel project={project} sendForReview={sendForReview} extendTime={extendTime}  versionIndex={versionIndex}  setVersionIndex={setVersionIndex}/>}
-      {user !== null && user?.customData?.role === "client" && <ClientManuscriptControlPanel project={project} approveManuscript={approveManuscript} askForRevision={askForRevision}  versionIndex={versionIndex}  setVersionIndex={setVersionIndex}/>}
-      <ManuscriptMetadata project={project}  versionIndex={versionIndex}/>
+      {/* {user !== null && user?.customData?.role === "freelancer" && <FreelancerManuscriptControlPanel project={project} sendForReview={sendForReview} extendTime={extendTime}  versionIndex={versionIndex}  setVersionIndex={setVersionIndex}/>} */}
+      {/* {user !== null && user?.customData?.role === "client" && <ClientManuscriptControlPanel project={project} approveManuscript={approveManuscript} askForRevision={askForRevision}  versionIndex={versionIndex}  setVersionIndex={setVersionIndex}/>} */}
+      <ManuscriptMetadata project={project}  versionIndex={versionIndex} setVersionIndex={setVersionIndex} user={user} approveManuscript={approveManuscript} askForRevision={askForRevision} sendForReview={sendForReview} extendTime={extendTime}/>
     </Stack>
     <Block inset >
       <ManuscriptScenes versionIndex={versionIndex}/>
@@ -149,46 +150,129 @@ function VersionSelect({versions, versionIndex, setVersionIndex}) {
   );
 }
 
+function ManuscriptOptionsButton() {
+
+  return(
+      <Box>
+          <Button popoverOpen="#manuscriptOptions" onClick={()=>{console.log("manuscript options button clicked")}}>
+              <MoreVertIcon/>
+          </Button>
+          <Popover id="manuscriptOptions" closeByOutsideClick className="more-popover-menu">
+              <List>
+                <ListItem>
+                  <Button
+                    popoverClose 
+                    onClick={()=>{
+                        // f7.dialog.confirm('Are you sure you want to delete scene '+index,()=>deleteScene(index))
+                    }} 
+                    iconF7="speaker_zzz_fill"
+                  >
+                    <Icon />
+                    Listen to AI Voiceover
+                  </Button>
+                </ListItem>
+                <ListItem>
+                  <Button
+                    popoverClose 
+                    onClick={()=>{
+                        // f7.dialog.confirm('Are you sure you want to delete scene '+index,()=>deleteScene(index))
+                    }} 
+                    iconF7="eye_fill"
+                  >
+                    <Icon />
+                    Preview
+                  </Button>
+                </ListItem>
+                <ListItem>
+                  <Button
+                    popoverClose 
+                    onClick={()=>{
+                        // f7.dialog.confirm('Are you sure you want to delete scene '+index,()=>deleteScene(index))
+                    }} 
+                    iconF7="arrow_down_to_line"
+                  >
+                    <Icon />
+                    Download PDF
+                  </Button>
+                </ListItem>          
+              </List>
+          </Popover>
+      </Box>
+  )
+}
+
 function ManuscriptMetadata (props){
-  const {project, versionIndex} = props
+  const {project, versionIndex, setVersionIndex, sendForReview,extendTime,approveManuscript,askForRevision,user} = props
   return(
     <Block inset strong style={{flexGrow:1}}>
-        <Stack direction="row" spacing={2}>
-         <Stack direction="row" spacing={1} sx={{alignItems:'center'}}>
-            <Typography  variant="subtitle1" color="text.secondary" component="div">Language</Typography>
-            <MUIChip color="secondary" variant="outlined" label={project?.manuscript.language} ></MUIChip>
+        <Stack direction="row" sx={{justifyContent:"space-between"}}>
+          <Stack direction="row" spacing={2}>
+            <Stack direction="row" spacing={1} sx={{alignItems:'center'}}>
+              <Typography  variant="subtitle1" color="text.secondary" component="div">Language</Typography>
+              <MUIChip color="secondary" variant="outlined" label={project?.manuscript.language} ></MUIChip>
+            </Stack>
+            <Stack direction="row" spacing={1} sx={{alignItems:'center'}}>
+              <Typography  variant="subtitle1" color="text.secondary" component="div">Scenes</Typography>
+              <MUIChip color="secondary" variant="outlined" label={project.manuscript.versions[versionIndex-1]?.scenes.length} ></MUIChip>
+            </Stack>
+            <Stack direction="row" sx={{alignItems:'center'}}>
+              <Typography mr={1} variant="subtitle1" color="text.secondary" component="div">Word count</Typography>
+              <MUIChip color="secondary" variant="outlined" label={"385"} ></MUIChip>
+            </Stack>
+            <Stack direction="row" spacing={1} sx={{alignItems:'center'}}>
+              <Typography variant="subtitle1" color="text.secondary" component="div">Target length</Typography>
+              <MUIChip color="secondary" variant="outlined" label={project?.brief.formResponse.answers[0].choice.label} ></MUIChip>
+              <MUIChip color="secondary" variant="outlined" label={project?.brief.formResponse.answers[1].choice.label} ></MUIChip>
+            </Stack>
           </Stack>
-          <Stack direction="row" spacing={1} sx={{alignItems:'center'}}>
-            <Typography  variant="subtitle1" color="text.secondary" component="div">Scenes</Typography>
-            <MUIChip color="secondary" variant="outlined" label={project.manuscript.versions[versionIndex-1]?.scenes.length} ></MUIChip>
+          <Stack direction="row" spacing={1}>
+            <VersionSelect versions={project?.manuscript.versions.map(version => version.id)} versionIndex={versionIndex} setVersionIndex={setVersionIndex}/>
+            {user !== null && user?.customData?.role === "client" && 
+              <Stack direction="row" spacing={1}>
+                {project.manuscript.status === "review" && <Box>
+                  <MUIButton size="small" variant="contained" color= "success" startIcon={<SendIcon />} onClick={()=>f7.dialog.confirm('Are you sure you want to approve?','Approve manuscript',approveManuscript)}>
+                    Approve
+                  </MUIButton>
+                </Box>}
+                {project.manuscript.status === "review" && <Box>
+                  <MUIButton size="small" variant="contained" color= "warning" startIcon={<MoreTimeIcon />} onClick={()=>f7.dialog.confirm('Are you sure you want to ask for revison?','Ask for revision',askForRevision)}>
+                    Ask for revision
+                  </MUIButton>
+                </Box>}
+                {project.manuscript.status === "open" && <Box>
+                  <MUIChip  color="secondary" variant="outlined" icon={<AccessTimeIcon/>} label="In revision by manuscript writer"/>
+                </Box>}
+                {project.manuscript.status === "approved" && <Box>
+                  <MUIChip  color="secondary" variant="outlined" icon={<AccessTimeIcon/>} label="Approved"/>
+                </Box>}
+              </Stack>
+            }
+            {user !== null && user?.customData?.role === "freelancer" && 
+              <Stack direction="row" spacing={1}>
+                {project.manuscript.status === "open" && <Box>
+                  <MUIButton size="small" variant="contained" color= "success" startIcon={<SendIcon />} 
+                    onClick={()=>{
+                      f7.dialog.confirm('Are you sure you want to send the manuscript to client for review?','Send manuscript to client',sendForReview)
+                    }}
+                  >
+                    Send to client
+                  </MUIButton>
+                </Box>}
+                {project.manuscript.status === "open" && <Box>
+                  <MUIButton size="small" variant="contained" color= "warning" startIcon={<MoreTimeIcon />} onClick={()=>f7.dialog.confirm('Are you sure you want to extend time?','Extend time',extendTime)}>
+                    Extend time
+                  </MUIButton>
+                </Box>}
+                {project.manuscript.status === "review" && <Box>
+                  <MUIChip  color="secondary" variant="outlined" icon={<AccessTimeIcon/>} label="In review by client"/>
+                </Box>}
+                {project.manuscript.status === "approved" && <Box>
+                  <MUIChip  color="success" variant="outlined" icon={<CheckIcon/>} label="Approved"/>
+                </Box>}
+              </Stack>
+            }
+            <ManuscriptOptionsButton />
           </Stack>
-          <Stack direction="row" sx={{alignItems:'center'}}>
-            <Typography mr={1} variant="subtitle1" color="text.secondary" component="div">Word count</Typography>
-            <MUIChip color="secondary" variant="outlined" label={"385"} ></MUIChip>
-          </Stack>
-          <Stack direction="row" spacing={1} sx={{alignItems:'center'}}>
-            <Typography variant="subtitle1" color="text.secondary" component="div">Target length</Typography>
-            <MUIChip color="secondary" variant="outlined" label={project?.brief.formResponse.answers[0].choice.label} ></MUIChip>
-            <MUIChip color="secondary" variant="outlined" label={project?.brief.formResponse.answers[1].choice.label} ></MUIChip>
-          </Stack>
-          
-        </Stack>
-        <Stack direction="row" mt={2} spacing={2}>
-          <Box>
-            <MUIButton size="small" variant="contained" color= "info" startIcon={<VolumeUpIcon />}>
-              Listen to AI Voiceover
-            </MUIButton>
-          </Box>
-          <Box>
-            <MUIButton size="small" variant="contained" color= "info" startIcon={<VisibilityIcon />}>
-              Preview
-            </MUIButton>
-          </Box>
-          <Box>
-            <MUIButton size="small" variant="contained" color= "info" startIcon={<PictureAsPdfIcon />}>
-            Download PDF
-            </MUIButton>
-          </Box>
         </Stack>
       </Block>
   )
